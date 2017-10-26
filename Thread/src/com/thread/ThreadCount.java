e1c58d21822a6778639ee45e0b9c04192793bf8e
liusupreme <33112409+liusupreme@users.noreply.github.com>
2017-10-26 21:14:59 +0800

    lh  test

---
+++ Thread/src/com/thread/ThreadCount.java
@@ -0,0 +1,26 @@
+package com.thread;
+
+public class ThreadCount {
+
+	public static void main(String[] args) {
+		int count = 0;
+		try {
+			while(true)
+			{
+				
+				Thread t = new Thread();
+				t.start();
+				if(count>0 && count/10000 ==0)
+				{
+					System.out.println("a:" + Thread.activeCount());
+				}
+				count++;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(">>>>>>>>>:" + Thread.activeCount());
+		System.out.println("-------------" + count);
+	}
+
+}
